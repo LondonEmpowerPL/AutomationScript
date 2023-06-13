015a893b0674b5990e3677c0b244e1115c6b7686
YLE05 <YLE05@SKYLT0530869>
2023-06-13 14:20:39 +0100

    Add project files.

---
+++ EmpowerAutomationScriptPL_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerAutomationScriptPL_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerAutomationScriptPL_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("23AB13EB-7A08-437B-8FBF-D6A01028B46F")]
+[assembly: AssemblyVersion("1.0.0.0")]
