ab0f0e305f1fa18b7983711f027e132b2204d0a0
YLE05 <paul.lee@sky.uk>
2023-06-13 14:24:20 +0100

    Changed information event message

--- EmpowerAutomationScriptPL_1/EmpowerAutomationScriptPL_1.cs
+++ EmpowerAutomationScriptPL_1/EmpowerAutomationScriptPL_1.cs
@@ -17,7 +17,7 @@ namespace EmpowerAutomationScriptPL_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World!");
+			engine.GenerateInformation("Hello World from Empower London!");
 		}
 	}
 }
